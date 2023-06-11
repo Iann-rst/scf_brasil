5d25c9d017536705e4ae6ea74ba05435522c97b0
Iann Rodrigues <iannrst11@gmail.com>
2023-06-11 20:42:24 -0300

    feat: Teste 6

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var teste2 = require("./teste2");
 var teste3 = require("./teste3");
 var teste4 = require("./teste4");
 var teste5 = require("./teste5");
+var session = require("./authenticate");
+const { verifyJWT } = require('./src/middlewares/verify-jwt');
 
 
 app.set('view engine', 'jade');
@@ -25,15 +27,19 @@ app.get('/', function(req, res){
   post users/ </br>
   delete users/ </br>
   put users/ </br>
+  post session/ </br>
   `);
 });
 
 app.get("/user", teste1.getUser);
 app.get("/users", teste1.getUsers);
 app.post("/users", teste2)
-app.delete("/users", teste3)
-app.put("/users", teste4)
+app.delete("/users", verifyJWT ,teste3)
+app.put("/users", verifyJWT, teste4)
 app.get("/users/access", teste5);
+
+// Realizar login, para poder acessar as rotas de deletar e atualizar usuário (Teste 6)
+app.post("/session", session)
 
 
 const port  = 3000;
