49fa4e38c76859905af6edaeea41aa14eebed889
Iann Rodrigues <iannrst11@gmail.com>
2023-06-11 20:21:14 -0300

    feat: Informar total de busca por um usuário

--- teste5.js
+++ teste5.js
@@ -1,9 +1,23 @@
+var data = require('./fakeData')
 
+
+// Buscar total de acesso do usuário, pelo email
 
 module.exports = function(req, res){
     
-    var name =  req.query.name;
+    var email =  req.query.email;
 
-    res.send("Usuário " +  name  + "  foi lido 0 vezes.");
+    if(!email){
+        return res.status(400).send('E-mail do usuário não foi informado!')
+    }
+
+    const user = data.find((user)=>user.email === email)
+    
+    if(!user){
+        return res.status(404).send('Usuário não encontrado!')
+    }
+
+
+    res.send(`Usuário ${user.name} foi lido ${user.views} vezes.`);
 
 };
